1a3ac2289b14f0e34d29b6a747f8d186c6a7a003
Johnny pot <arroga077@gmail.com>
2021-11-15 17:17:59 +0700

    Добавьте файлы проекта.

---
+++ ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGLab4
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel()
+        {
+
+        }
+
+
+        #region Notify
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        #endregion
+    }
+}
